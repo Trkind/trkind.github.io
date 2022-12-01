ff6b0d182dc5fe9afc1eeaddc64a0309f6933940
kju <tristan_kindle@gmx.com>
2022-12-01 16:00:53 +0100

    searchAPIadded

---
+++ script.js
@@ -0,0 +1,59 @@
+window.addEventListener("load", () => {
+    function sendData(searchParams) {
+      const XHR = new XMLHttpRequest();
+  
+      // Bind the FormData object and the form element
+      const FD = new FormData(form);
+  
+      // Define what happens on successful data submission
+      XHR.addEventListener("load", (event) => {
+
+        const myNode = document.getElementById("results");
+        while (myNode.firstChild) {
+          myNode.removeChild(myNode.lastChild);
+        }
+
+        let answer = JSON.parse(event.target.responseText).suggestions
+
+        for (const a of answer){
+          myNode.appendChild(document.createTextNode(a))
+          myNode.appendChild(document.createElement("br"))
+        }
+
+        
+        
+        document.getElementById("results").value = list
+      });
+  
+      // Define what happens in case of error
+      XHR.addEventListener("error", (event) => {
+        alert('Oops! Something went wrong.');
+      });
+  
+      // Set up our request
+      let url = "https://www.zefix.ch/ZefixREST/api/v1/autocomplete.json?"
+      url += searchParams
+      
+      XHR.open("GET", url);
+  
+      // The data sent is what the user provided in the form
+      XHR.send(FD);
+    }
+  
+    // Get the form element
+    const form = document.getElementById("searchForm");
+  
+    // Add 'submit' event handler
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let searchString = document.getElementById("searchString").value
+      let searchDeletedEntries = document.getElementById("searchDeletedEntries").checked
+      let searchInOldNames = document.getElementById("searchInOldNames").checked
+
+      const paramsObj = {prefix: searchString, deletedFirms: searchDeletedEntries, formerNames: searchInOldNames};
+      const searchParams = new URLSearchParams(paramsObj);
+      console.log(searchParams.toString())
+      sendData(searchParams);
+    });
+  });
+
